36cf91f25814c209f29bf26b9c176d4a76a450a6
tthyb <tthyb123@126.com>
2020-02-25 17:56:11 +0800

    'eureka'

---
+++ spring-eureka/src/main/java/com/huyibo/springeureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.huyibo.springeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by huyibo on 2020/2/25.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+
+}
--- spring-logstash/src/main/java/com/huyibo/springlogstash/SlsApplication.java
+++ spring-logstash/src/main/java/com/huyibo/springlogstash/SlsApplication.java
@@ -3,11 +3,13 @@ package com.huyibo.springlogstash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by huyibo on 2020/2/11.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SlsApplication {
 
     public static void main(String[] args){
